2be66dbe94fd6968bedce6435920d8bd4733b39d
Anders Kagervall <anders.kagervall@gmail.com>
2020-01-31 14:18:37 +0100

    Doc fixes

--- R/package-ICimportr.R
+++ R/package-ICimportr.R
@@ -1,9 +1,14 @@
-#' ICimportr: A package helping in the tedious to create InterCatch import fiules
+#' ICimportr: A package helping you in the tedious to create InterCatch import files
 #'
-#' The foo package provides ...
+#' The ICimportr package provides functions to format HI, SI and SD records according to the
+#' InterCatch Exchange format. The package alse contains two data tables \code{fishing_area}
+#' and \code{fleet_key}. The tables can be used to assign metiers according to gear type and
+#' to set correct SubDiv-codes as required by ICES. The tables might be tuned for Swedish
+#' conditions but I am open for suggestions.
 #'
 #' @section ICimportr functions:
-#' The foo functions ...
+#' The ICimportr functions are \code{\link{HI_record()}}, \code{\link{SI_record()}} and
+#' \code{\link{SD_record}}:
 #'
 #' @docType package
 #' @name ICimportr
